d1fa0b2ec73b231b8ca5ce4e64809a2f915600eb
Bora <borabalos9@gmail.com>
2023-08-11 01:50:24 +1000

    Styling for gallery

--- bnsb/src/components/Gallery/Index.jsx
+++ bnsb/src/components/Gallery/Index.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './style.module.css';
 
 const images = [
@@ -7,7 +8,29 @@ const images = [
 export default function Index() {
     return (
         <div className={styles.gallery}>
+            <div className='galleryBody'>
+                <Column images={[images[0], images[1], images[2]]} />
+                <Column images={[images[3], images[4], images[5]]} />
+                <Column images={[images[6], images[7], images[8]]} />
+            </div>
+        </div>
+    )
+}
 
+const Column = ({images}) => {
+    return (
+        <div className={styles.column}>
+            {
+                images.map((src, index) => {
+                    return <div key={index} className={styles.imageContainer}>
+                        <Image 
+                            src={`/images/${src}`}
+                            fill
+                            alt="image"
+                        />
+                    </div>
+                })
+            }
         </div>
     )
 }
