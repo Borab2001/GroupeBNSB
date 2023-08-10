7e2c3373073240b9a242f8947759985a0ad1ef1c
Bora <borabalos9@gmail.com>
2023-08-11 01:33:18 +1000

    Starting the gallery animation

---
+++ bnsb/src/components/Gallery/Index.jsx
@@ -0,0 +1,13 @@
+import styles from './style.module.css';
+
+const images = [
+    "" //images here
+]
+
+export default function Index() {
+    return (
+        <div className={styles.gallery}>
+
+        </div>
+    )
+}
