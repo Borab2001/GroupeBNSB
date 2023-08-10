14cd58dbe4cc970fcbc7645aaf7c8b6077cd7e39
Bora <borabalos9@gmail.com>
2023-08-11 01:24:53 +1000

    gsap to finish text text gradient opacity animation

--- bnsb/src/components/Phrases/Index.jsx
+++ bnsb/src/components/Phrases/Index.jsx
@@ -1,7 +1,35 @@
+'use client';
+
 import styles from './style.module.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from 'react';
 
 export default function Phrase() {
     const phrase = "Innovation translates an idea into a reality in Business, Process and Product. We help company to create a new fashion system with high degree of flexibility through innovation and sourcing process with our designers and experts.";
+
+    const refs = useRef([])
+    const container = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        createAnimations()
+    }, []);
+
+    const createAnimations = () => {
+        gsap.to(refs.current, {
+            opacity: 1,
+            ease: "none",
+            stagger: 0.1,
+            scrollTrigger: {
+                trigger: container.current,
+                start: 'top',
+                end: `+=${window.innerHeight / 1.5}`,
+                markers: false,
+                scrub: true
+            }
+        })
+    }
 
     const splitWords = () => {
         let body = []
@@ -15,16 +43,18 @@ export default function Phrase() {
     const splitLetters = (word) => {
         let letters = [];
         word.split("").forEach((letter, index) => {
-            letters.push(<span key={`letter_${index}`}>{letter}</span>)
+            letters.push(<span ref={el => {refs.current.push(el)}} key={`letter_${index}`}>{letter}</span>)
         })
         return letters;
     }
 
     return (
-        <div className={styles.phrase}>
-            {
-                splitWords()
-            }
+        <div ref={container} className={styles.phrase}>
+            <div className={styles.phraseBody}>
+                {
+                    splitWords()
+                }
+            </div>
         </div>
     )
 }
