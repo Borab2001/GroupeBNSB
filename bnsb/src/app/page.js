7cbc651b8a2767962258aa1671bfb50b6d7711b1
Bora <borabalos9@gmail.com>
2023-08-11 11:04:25 +1000

    Lenis Scroll added for smooth scrolling

--- bnsb/src/app/page.js
+++ bnsb/src/app/page.js
@@ -1,7 +1,25 @@
 import Phrase from '@/components/Phrases/Index'
 import styles from './page.module.css'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
 
 export default function Home() {
+  
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    // lenis.on('scroll', (e) => {
+    //   console.log(e)
+    // })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.body}>
