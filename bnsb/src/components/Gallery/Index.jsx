2fa2d588aa61c51722d89e817f2fbf271ca3543e
Bora <borabalos9@gmail.com>
2023-08-12 13:56:47 +1000

    Changed gallery image formats + color adjustment for desc

--- bnsb/src/components/Gallery/Index.jsx
+++ bnsb/src/components/Gallery/Index.jsx
@@ -8,18 +8,18 @@ import Image from 'next/image';
 import Lenis from '@studio-freight/lenis';
 
 const images = [
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
+    "1.webp",
+    "2.webp",
+    "3.webp",
+    "4.webp",
+    "5.webp",
+    "6.webp",
+    "7.webp",
+    "8.webp",
     "9.webp",
-    "10.jpg",
-    "11.jpg",
-    "12.jpg"
+    "10.webp",
+    "11.webp",
+    "12.webp"
 ]
 
 export default function Index() {
