232176569922d131672ddbf80c3a696f6d049f51
Bora <borabalos9@gmail.com>
2023-08-12 20:16:23 +1000

    Preloader text and opacity animated

--- bnsb/src/components/Preloader/Index.jsx
+++ bnsb/src/components/Preloader/Index.jsx
@@ -1,12 +1,25 @@
 import styles from './style.module.css';
 import { motion } from 'framer-motion';
-import { slideUp } from './preloadAnim';
+import { opacity, slideUp } from './preloadAnim';
+import { useEffect, useState } from 'react';
 
 export default function Index() {
+
+    const [index, setIndex] = useState(0);
+    const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallå", "Guten tag", "Hallo"];
+
+    useEffect(() => {
+        if(index == words.length -1) return;
+        
+        setTimeout(() => {
+            setIndex(index + 1);
+        }, index == 0 ? 1000 : 150)
+    
+    }, [index]);
     
     return (
         <motion.div variants={slideUp} initial="initial" exit="exit" className={styles.intro}>
-            <p>Is Loading</p>
+            <motion.p variants={opacity} initial="initial" animate="enter">{words[index]}</motion.p>
         </motion.div>
     )
 }
