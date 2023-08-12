7e8b8eef9cd7570bf092ce76c0e80e7d94ffc4a6
Bora <borabalos9@gmail.com>
2023-08-12 11:14:00 +1000

    Added few images for test, fixed animation

--- bnsb/src/app/page.js
+++ bnsb/src/app/page.js
@@ -1,32 +1,31 @@
 "use client";
 
-import Phrase from '@/components/Phrases/Index'
-import styles from './page.module.css'
-import Lenis from '@studio-freight/lenis'
-import { useEffect } from 'react'
+import Phrase from '@/components/Phrases/Index';
+import Gallery from '@/components/Gallery/Index';
+import styles from './page.module.css';
+
+import Lenis from '@studio-freight/lenis';
+import { useEffect } from 'react';
 
 export default function Home() {
   
-  useEffect(() => {
-    const lenis = new Lenis()
+	useEffect(() => {
+		const lenis = new Lenis()
 
-    // lenis.on('scroll', (e) => {
-    //   console.log(e)
-    // })
+		function raf(time) {
+		lenis.raf(time)
+		requestAnimationFrame(raf)
+		}
 
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-  }, []);
+		requestAnimationFrame(raf)
+	}, []);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.body}>
-        <Phrase />
-      </div>
-    </main>
-  )
+		<main className={styles.main}>
+			<div className={styles.body}>
+				<Gallery />
+				<Phrase />
+			</div>
+		</main>
+	)
 }
--- bnsb/src/components/Gallery/Index.jsx
+++ bnsb/src/components/Gallery/Index.jsx
@@ -1,56 +1,91 @@
-import Image from 'next/image';
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import { useTransform, useScroll, motion } from 'framer-motion';
+
 import styles from './style.module.css';
-import { motion, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
-import useDimension from '@/app/hooks/useDimension';
+import Image from 'next/image';
+import Lenis from '@studio-freight/lenis';
 
 const images = [
-    "" //images here
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.webp",
+    "8.jpg",
+    "9.webp"
 ]
 
-export default function Index() {
-    
-    const container = useRef(null);
-    const { height } = useDimension();
+export default function Gallery() {
+  
+    const gallery = useRef(null);
+    const [dimension, setDimension] = useState({width:0, height:0});
+
     const { scrollYProgress } = useScroll({
-        target: container,
+        target: gallery,
         offset: ['start end', 'end start']
     })
 
+    const { height } = dimension;
     const y1 = useTransform(scrollYProgress, [0, 1], [0, height * 2])
     const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3])
     const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25])
-    const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
 
+
+    useEffect(() => {
+        const lenis = new Lenis()
+
+        const raf = (time) => {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+
+        const resize = () => {
+            setDimension({width: window.innerWidth, height: window.innerHeight})
+        }
+
+        window.addEventListener("resize", resize)
+        requestAnimationFrame(raf);
+        resize();
+
+        return () => {
+            window.removeEventListener("resize", resize);
+        }
+    }, [])
 
     return (
-        <div className={styles.gallery}>
-            <div className={styles.gallerySpacer}></div>
-            <div ref={container} className='galleryBody'>
-                <Column images={[images[0], images[1], images[2]]} y={y1} />
-                <Column images={[images[3], images[4], images[5]]} y={y2} />
-                <Column images={[images[6], images[7], images[8]]} y={y3} />
-                <Column images={[images[9], images[10], images[11]]} y={y4} />
+        <div>
+            <div className={styles.spacer}></div>
+            <div ref={gallery} className={styles.gallery}>
+                <Column images={[images[0], images[1], images[2]]} y={y1}/>
+                <Column images={[images[3], images[4], images[5]]} y={y2}/>
+                <Column images={[images[6], images[7], images[8]]} y={y3}/>
             </div>
-            <div className={styles.gallerySpacer}></div>
+            {/* <div className={styles.spacer}></div> */}
         </div>
     )
 }
 
-const Column = ({images, y=0}) => {
+const Column = ({images, y}) => {
     return (
-        <motion.div style={{y}} className={styles.column}>
-            {
-                images.map((src, index) => {
-                    return <div key={index} className={styles.imageContainer}>
-                        <Image 
-                            src={`/images/${src}`}
-                            fill
-                            alt="image"
-                        />
-                    </div>
-                })
-            }
+        <motion.div 
+            className={styles.column}
+            style={{y}}
+        >
+        {
+            images.map( (src, i) => {
+            return <div key={i} className={styles.imageContainer}>
+                <Image 
+                    src={`/images/${src}`}
+                    alt='image'
+                    fill
+                />
+            </div>
+            })
+        }
         </motion.div>
     )
 }
