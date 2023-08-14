32cb47d1ff45c7285531e87fac87ddfb893d607d
Bora <borabalos9@gmail.com>
2023-08-14 14:44:38 +1000

    Cleaning + centering phrase + assets

--- bnsb/src/components/Phrase/Index.jsx
+++ bnsb/src/components/Phrase/Index.jsx
@@ -8,30 +8,7 @@ import { useEffect, useRef } from 'react';
 export default function Index() {
     
     // PHRASE FOR ONE PARAGRAPH
-    // const phrase = "Innovation translates an idea into a reality in Business, Process and Product. We help company to create a new fashion system with high degree of flexibility through innovation and sourcing process with our designers and experts.";
     const phrase = "Innovation translates an idea into reality ";
-
-    // PHRASE FOR MULRIPLE PARAGRAPHS
-    // const phrases = [
-    //     "Innovation translates an idea into a reality in Business, Process and Product. We help company to create a new fashion system with high degree of flexibility through innovation and sourcing process with our designers and experts.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida.",
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo a augue iaculis gravida."
-    // ];
 
     const refs = useRef([])
     const container = useRef(null);
