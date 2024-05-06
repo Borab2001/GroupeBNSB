ccb4062c16a525b936db1aeac0a449ae345a269f
Bora <borabalos9@gmail.com>
2024-05-06 12:21:56 +0200

    Basic hero content

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -1,7 +1,12 @@
 const Hero = () => {
     return (
         <div className="h-screen flex items-center justify-center text-3xl">
-            Hero
+            <h1>
+                Hero
+            </h1>
+            <h2>
+                For a sustainable and eco-friendly fashion
+            </h2>
         </div>
     );
 }
