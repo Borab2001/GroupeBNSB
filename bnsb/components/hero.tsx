9505ff32c8f6e8e6e3f3d00b7e9d207848ed627d
Bora <borabalos9@gmail.com>
2024-05-06 13:42:51 +0200

    Styling for hero

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -1,10 +1,10 @@
 const Hero = () => {
     return (
-        <div className="h-svh flex flex-col gap-4 items-center justify-center text-3xl">
-            <h1>
+        <div className="h-lvh flex flex-col gap-4 items-center justify-center">
+            <h1 className="text-3xl text-balance">
                 Hero
             </h1>
-            <h2>
+            <h2 className="text-xl text-center text-balance">
                 For a sustainable and eco-friendly fashion
             </h2>
         </div>
