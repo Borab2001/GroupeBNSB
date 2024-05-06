20d1e2b7a1bb0f2fc669b50b1e92f98ebf9b6e33
Bora <borabalos9@gmail.com>
2024-05-06 13:17:00 +0200

    Svh height for hero + additionnal styles

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <div className="h-screen flex items-center justify-center text-3xl">
+        <div className="h-svh flex flex-col gap-4 items-center justify-center text-3xl">
             <h1>
                 Hero
             </h1>
