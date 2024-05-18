0df5d274ee20919976597a38682a79d969be88f9
Bora <borabalos9@gmail.com>
2024-05-18 18:10:30 +0200

    Gradient to hero title

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -1,11 +1,11 @@
 const Hero = () => {
     return (
         <div className="h-lvh flex flex-col gap-4 items-center justify-center">
-            <h1 className="text-3xl text-balance">
-                Hero
+            <h1 className="text-[max(4vw,48px)] leading-16 p-4 text-balance text-center font-semibold bg-gradient-to-b from-black to-neutral-500 text-transparent bg-clip-text">
+                Biodiversity Nature Sustainability Business
             </h1>
             <h2 className="text-xl text-center text-balance">
-                For a sustainable and eco-friendly fashion
+                For Eco-Friendly Fashion
             </h2>
         </div>
     );
