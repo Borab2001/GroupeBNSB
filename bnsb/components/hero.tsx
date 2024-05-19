0f55c7c9873973709715c883eb03bca4b1993e6b
Bora <borabalos9@gmail.com>
2024-05-19 17:56:29 +0200

    Fixed weird zustand error from lenis + styling for hero

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -8,8 +8,8 @@ const Hero = () => {
     const router = useRouter();
 
     return (
-        <div className="h-lvh flex flex-col gap-4 items-center justify-center">
-            <h1 className="text-[max(4vw,48px)] leading-16 p-4 text-balance text-center font-semibold bg-gradient-to-b from-black to-slate-700 text-transparent bg-clip-text">
+        <div className="max-w-3xl mx-auto h-lvh flex flex-col gap-4 p-4 items-center justify-center">
+            <h1 className="text-[max(4vw,32px)] leading-16 text-balance text-center font-semibold bg-gradient-to-b from-black to-slate-700 text-transparent bg-clip-text">
                 Biodiversity Nature Sustainability Business
             </h1>
             <h2 className="text-[max(2vw,24px)] text-center text-balance">
@@ -19,9 +19,9 @@ const Hero = () => {
                 onClick={() => router.push("/contact")}
                 variant="default"
                 size="lg"
-                className="rounded-full shadow-[inset_0_1px_0_rgba(255,255,255,0.25),_0_4px_4px_rgba(150,150,150,0.25),_0_1px_0_rgb(31,31,38),_0_10px_15px_-3px_rgba(0,0,0,0.1)]"
+                className="mt-4 text-md rounded-full shadow-[inset_0_1px_0_rgba(255,255,255,0.25),_0_4px_4px_rgba(150,150,150,0.25),_0_1px_0_rgb(31,31,38),_0_10px_15px_-3px_rgba(0,0,0,0.1)]"
             >
-                Contact
+                Contact Us
             </Button>
         </div>
     );
