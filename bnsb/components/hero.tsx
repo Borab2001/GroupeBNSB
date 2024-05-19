51b08469a249f7dd19497cef5b5c870d0e7c65f3
Bora <borabalos9@gmail.com>
2024-05-19 02:24:10 +0200

    Content in hero component

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -4,8 +4,8 @@ const Hero = () => {
             <h1 className="text-[max(4vw,48px)] leading-16 p-4 text-balance text-center font-semibold bg-gradient-to-b from-black to-slate-700 text-transparent bg-clip-text">
                 Biodiversity Nature Sustainability Business
             </h1>
-            <h2 className="text-xl text-center text-balance">
-                For Eco-Friendly Fashion
+            <h2 className="text-[max(2vw,24px)] text-center text-balance">
+                Innovation and Excellence in Design and Sourcing
             </h2>
         </div>
     );
