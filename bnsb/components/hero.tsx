0627e2be4e731c955b779ec125435791d49780f9
Bora <borabalos9@gmail.com>
2024-05-19 02:19:45 +0200

    Card & hero styling

--- bnsb/components/hero.tsx
+++ bnsb/components/hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
         <div className="h-lvh flex flex-col gap-4 items-center justify-center">
-            <h1 className="text-[max(4vw,48px)] leading-16 p-4 text-balance text-center font-semibold bg-gradient-to-b from-black to-neutral-500 text-transparent bg-clip-text">
+            <h1 className="text-[max(4vw,48px)] leading-16 p-4 text-balance text-center font-semibold bg-gradient-to-b from-black to-slate-700 text-transparent bg-clip-text">
                 Biodiversity Nature Sustainability Business
             </h1>
             <h2 className="text-xl text-center text-balance">
