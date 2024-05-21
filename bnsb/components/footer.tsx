71380e104fa913958d615fec8228aedbf473ed58
Bora <borabalos9@gmail.com>
2024-05-22 01:21:40 +0200

    Styling to footer + comment

--- bnsb/components/footer.tsx
+++ bnsb/components/footer.tsx
@@ -8,10 +8,11 @@ const Footer = () => {
             style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
         > {/* my-8 */}
             <div className="relative h-[calc(100vh+800px)] -top-[100vh]">
-                <div className="sticky top-[calc(100vh-800px)] h-[800px]">
-                <span className="inline-flex animate-text-gradient bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] text-3xl text-center text-transparent font-medium bg-clip-text">
-                    Groupe BNSB
-                </span>
+                <div className="sticky top-[calc(100vh-800px)] h-[800px] flex flex-col justify-center items-center gap-12">
+                    <span className="inline-flex animate-text-gradient bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] text-3xl md:text-9xl text-center text-transparent font-medium bg-clip-text">
+                        Groupe BNSB
+                    </span>
+                    {/* TODO: Magnetic socials icons here */}
                     <p className="w-full text-center">&#169; Groupe BNSB {currentYear}. All rights reserved</p>
                 </div>
             </div>
