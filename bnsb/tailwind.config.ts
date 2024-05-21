d84ce8e04dab4f434f3629012e3aac0e4f05dae1
Bora <borabalos9@gmail.com>
2024-05-22 01:12:02 +0200

    Tailwind config for gradient text animation in footer

--- bnsb/tailwind.config.ts
+++ bnsb/tailwind.config.ts
@@ -68,10 +68,18 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "text-gradient": {
+          "to": {
+            "backgroundPosition": "200% center"
+          }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "animation": {
+          "text-gradient": "text-gradient 1.5s linear infinite"
+        },
       },
     },
   },
