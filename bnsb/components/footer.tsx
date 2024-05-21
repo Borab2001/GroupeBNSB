362ee147c9bf2f6c9a6b8c81c5c6b4932fdbe729
Bora <borabalos9@gmail.com>
2024-05-22 01:10:55 +0200

    UI for gradient animated text in footer

--- bnsb/components/footer.tsx
+++ bnsb/components/footer.tsx
@@ -9,6 +9,9 @@ const Footer = () => {
         > {/* my-8 */}
             <div className="relative h-[calc(100vh+800px)] -top-[100vh]">
                 <div className="sticky top-[calc(100vh-800px)] h-[800px]">
+                <span className="inline-flex animate-text-gradient bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] text-3xl text-center text-transparent font-medium bg-clip-text">
+                    Groupe BNSB
+                </span>
                     <p className="w-full text-center">&#169; Groupe BNSB {currentYear}. All rights reserved</p>
                 </div>
             </div>
