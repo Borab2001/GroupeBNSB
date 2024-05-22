6d22f0e80e418aca890fe9135adf4c1ac4235d2a
Bora <borabalos9@gmail.com>
2024-05-22 12:14:24 +0200

    Tracking mouse movement for magnetic icon

--- bnsb/components/ui/magnetic-icon.tsx
+++ bnsb/components/ui/magnetic-icon.tsx
@@ -1,19 +1,44 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, spring } from "framer-motion";
+import { useRef, useState } from "react";
 
 interface MagneticIconProps {
     children: React.ReactNode;
 }
 
-const MagneticIcon: React.FC<MagneticIconProps> = ({
-    children
-}) => {
+const MagneticIcon: React.FC<MagneticIconProps> = ({ children }) => {
+
+    const ref = useRef<HTMLDivElement | null>(null);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const mouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (ref.current) {
+            const { clientX, clientY } = e;
+            const { width, height, left, top } = ref.current.getBoundingClientRect();
+            const x = clientX - (left + width / 2);
+            const y = clientY - (top + height / 2);
+            setPosition({ x, y });
+        }
+    }
+
+    const mouseLeave = () => {
+        setPosition({ x: 0, y: 0 });
+    }
+
+    const { x, y } = position;
+
     return (
-        <div>
+        <motion.div
+            onMouseMove={mouseMove}
+            onMouseLeave={mouseLeave}
+            ref={ref}
+            animate={{ x, y }}
+            transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+        >
             {children}
-        </div>
+        </motion.div>
     );
 }
- 
+
 export default MagneticIcon;
