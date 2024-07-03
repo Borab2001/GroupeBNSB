b3c1208cbcd7692f08a5d4afec26483ca12a99fb
Bora <borabalos9@gmail.com>
2024-07-03 14:58:47 +0200

    Upgraded Next version and fixed audit

--- bnsb/app/layout.tsx
+++ bnsb/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Groupe BNSB - Design &amp; Sourcing Office",
+	title: "Groupe BNSB - Design & Sourcing Office",
 	description: "Groupe BNSB supports all players in the textile sector in their eco-responsible transition to enable the textile industry to be more sustainable.",
 	keywords: "fashion, sustainability, product passport, design, sourcing, biodiversity, eco-friendly, textile"
 };
