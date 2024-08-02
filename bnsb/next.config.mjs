5fe6b74d88e16f18c13d6e145b34dd491d5bcd16
Bora <borabalos9@gmail.com>
2024-08-02 14:08:59 +0200

    Installed PWA

--- bnsb/next.config.mjs
+++ bnsb/next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withPWA = require('next-pwa')
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    pwa: {
+      dest: 'public', // Specify the destination directory for the service worker and other PWA files
+      disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+    },
+};
+
+// export default nextConfig;
+module.exports = withPWA(nextConfig);
