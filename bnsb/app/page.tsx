02738233303166383002e13a65fc26cb4c60ecfd
Bora <borabalos9@gmail.com>
2024-08-10 03:42:15 +0200

    Remove unnecessary imports

--- bnsb/app/page.tsx
+++ bnsb/app/page.tsx
@@ -1,18 +1,14 @@
-import Cards from "@/components/cards";
 import Gallery from "@/components/gallery";
 import Hero from "@/components/hero";
 import Team from "@/components/team";
-import TextRevealByWord from "@/components/ui/text-reveal";
 
 export default function Home() {  
 
   	return (
 		<main>
 			<Hero />
-			{/* <Cards /> */}
 			<Team />
 			<Gallery />
-			{/* <TextRevealByWord text={"Lorem ipsum dolor this is a text reveal animation text"} /> */}
 		</main>
   	);
 }
