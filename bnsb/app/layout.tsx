2d153191851483440f80515e9d3de1f2be3b98e2
Bora <borabalos9@gmail.com>
2024-10-15 14:55:55 +0200

    Fixing eslint while importing vercel analytics

--- bnsb/app/layout.tsx
+++ bnsb/app/layout.tsx
@@ -1,6 +1,7 @@
+// eslint-disable-next-line import/no-unresolved
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
 		<body className={inter.className}>
 			{children}
+			<Analytics />
 		</body>
     </html>
   );
