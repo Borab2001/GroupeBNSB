4de850e1802303ff7f946a784e7a52052d663fac
Bora <borabalos9@gmail.com>
2024-11-15 18:55:10 +0100

    Change notification bar message

--- bnsb/components/notification-bar.tsx
+++ bnsb/components/notification-bar.tsx
@@ -3,9 +3,7 @@ import Link from "next/link";
 const NotificationBar = () => {
     return (
         <div className="fixed top-0 z-30 w-full bg-red-500 text-center text-white text-sm sm:text-md">
-            <p className="py-2">🚀 The website is currently in build, please use this one
-                <Link className="ml-1 underline" href="https://groupe-bnsb.com/">here</Link>
-            </p>
+            <p className="py-2">🚀 This website is still under construction</p>
         </div>
     );
 }
