1398deb4fa1d9911db0e4319c49ccf7443dd5185
Bora <borabalos9@gmail.com>
2024-11-15 16:11:53 +0100

    test

--- bnsb/actions/index.ts
+++ bnsb/actions/index.ts
@@ -15,7 +15,7 @@ export const SendEmail = async (values: z.infer<typeof EmailSchema>) => {
 
         const { data, error } = await resend.emails.send({
             from: 'BNSB Website <onboarding@resend.dev>',
-            to: ['borabalos9@gmail.com'],
+            to: 'dianeanhduong@gmail.com',
             subject: 'New Contact Submission',
             replyTo: values.email,
             react: React.createElement(BNSBContactEmail, {
