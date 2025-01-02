704e83443d6167e5e50be2e80e0c7da1dace4fb6
Bora <borabalos9@gmail.com>
2025-01-02 23:37:27 +0100

    Simple header

--- bnsb/app/page.tsx
+++ bnsb/app/page.tsx
@@ -1,13 +1,21 @@
 export default function Home() {
     return (
-        <div className="w-full flex flex-1 flex-grow h-full p-4 flex-col justify-center items-center">
-			<div className="flex gap-4 flex-col">
-				<h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-					A more sustainable future for textile
-				</h1>
-				<p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
-					Design and sourcing office for sustainable textiles. We are a sourcing experts with 25+ years of experience in fashion, aiming for a more sustainable future for textiles. We work with brands, retailers, and manufacturers to develop sustainable textile solutions that are better for people and the planet.
+        <div className="w-full flex flex-1 flex-col flex-grow h-full p-4 ">
+			<header className="w-full flex flex-row justify-between p-4 uppercase">
+				<p>
+					Groupe BNSB
 				</p>
+				<div>Menu</div>
+			</header>
+			<div className="w-full flex flex-1 flex-col justify-center items-center">
+				<div className="flex gap-4 flex-col">
+					<h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
+						A more sustainable future for textile
+					</h1>
+					<p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
+						We are a design and sourcing office with experts over 25 years of experience in fashion, aiming for a more sustainable future. We work with brands, retailers, and manufacturers to develop sustainable textile solutions that are better for people and the planet.
+					</p>
+				</div>
 			</div>
         </div>
     );
