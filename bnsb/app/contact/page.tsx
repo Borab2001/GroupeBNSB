b2a77a40c7b2ae10907e3539889ab504c42dd841
Bora <borabalos9@gmail.com>
2025-01-03 22:38:23 +0100

    Contact form added to contact page with resend api key

--- bnsb/app/contact/page.tsx
+++ bnsb/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import { ContactForm } from "@/components/contact-form";
+
 const Contact = () => {
     return (
         <div className="w-full flex flex-1 flex-col flex-grow h-full p-4 ">
@@ -6,7 +8,10 @@ const Contact = () => {
                     <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
                         Let&apos;s get in touch
                     </h1>
-                    <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">You can reach us any time</p>
+                    <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
+                        You can reach us any time
+                    </p>
+                    <ContactForm />
                 </div>
             </div>
         </div>
