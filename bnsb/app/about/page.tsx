acb31ca6b6006d2d036d21fd6f3853003c592d8b
Bora <borabalos9@gmail.com>
2025-01-20 17:49:14 +0100

    Refactoring experience data in about, mapping and styling

--- bnsb/app/about/page.tsx
+++ bnsb/app/about/page.tsx
@@ -1,20 +1,43 @@
 const About = () => {
+
+    const experiences = [
+        {
+            year: '2021 - Present',
+            title: 'Foundation of BNSB',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum ex nobis fugiat nam vel voluptatibus deserunt iusto.'
+        },
+        {
+            year: '2021 - Present',
+            title: 'Foundation of BNSB',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum ex nobis fugiat nam vel voluptatibus deserunt iusto.'
+        },
+        {
+            year: '2021 - Present',
+            title: 'Foundation of BNSB',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum ex nobis fugiat nam vel voluptatibus deserunt iusto.'
+        }
+    ];
+
     return (
-        <div className="p-4">
-            <h1 className="text-8xl py-24 font-medium">
+        <div className="p-4 md:p-8">
+            <h1 className="text-5xl md:text-8xl py-24 font-medium">
                 About
             </h1>
             
-            <div className="w-full flex flex-row items-start justify-between">
-                <p className="text-2xl font-medium">
-                    2021 - Present
-                </p>
-                <div className="md:max-w-xl flex flex-col gap-2">
-                    <h2 className="text-3xl font-medium">Foundation of BNSB</h2>
-                    <p className="text-muted-foreground text-lg md:text-xl leading-relaxed">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum ex nobis fugiat nam vel voluptatibus deserunt iusto.
-                    </p>
-                </div>
+            <div className="w-full flex flex-col gap-10">
+                {experiences.map((experience, index) => (
+                    <div key={index} className="w-full flex flex-row gap-8 items-start justify-between">
+                        <p className="text-xl md:text-3xl font-medium">
+                            {experience.year}
+                        </p>
+                        <div className="md:max-w-xl flex flex-col gap-2">
+                            <h2 className="text-xl md:text-3xl font-medium">{experience.title}</h2>
+                            <p className="text-muted-foreground text-md md:text-lg leading-relaxed">
+                                {experience.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
