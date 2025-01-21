52a31eab42c0427797400dfd0eb892cb3987acba
Bora <borabalos9@gmail.com>
2025-01-21 14:18:13 +0100

    About layout styling

--- bnsb/app/about/page.tsx
+++ bnsb/app/about/page.tsx
@@ -20,24 +20,26 @@ const About = () => {
 
     return (
         <div className="p-4 md:p-8">
-            <h1 className="text-5xl md:text-8xl py-24 font-medium">
-                About
-            </h1>
-            
-            <div className="w-full flex flex-col gap-10">
-                {experiences.map((experience, index) => (
-                    <div key={index} className="w-full flex flex-row gap-8 items-start justify-between">
-                        <p className="text-xl md:text-3xl font-medium">
-                            {experience.year}
-                        </p>
-                        <div className="md:max-w-xl flex flex-col gap-2">
-                            <h2 className="text-xl md:text-3xl font-medium">{experience.title}</h2>
-                            <p className="text-muted-foreground text-md md:text-lg leading-relaxed">
-                                {experience.description}
+            <div className="flex flex-col gap-y-24 py-24 border-b border-muted-foreground">
+                <h1 className="text-5xl md:text-8xl font-medium">
+                    About
+                </h1>
+                
+                <div className="w-full flex flex-col gap-24">
+                    {experiences.map((experience, index) => (
+                        <div key={index} className="w-full flex flex-row gap-8 items-start justify-between">
+                            <p className="text-xl md:text-3xl font-medium">
+                                {experience.year}
                             </p>
+                            <div className="md:max-w-xl flex flex-col gap-2">
+                                <h2 className="text-xl md:text-3xl font-medium">{experience.title}</h2>
+                                <p className="text-muted-foreground text-md md:text-lg leading-relaxed">
+                                    {experience.description}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
