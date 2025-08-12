7771d4fe92b9c0e3de5221c7b4dc8c03e8bcc7f7
Bora <borabalos9@gmail.com>
2025-08-12 17:40:43 +0200

    feat: Horizontal gallery with koton images

--- bnsb/components/horizontal-scroll.tsx
+++ bnsb/components/horizontal-scroll.tsx
@@ -4,21 +4,51 @@ import { useRef } from 'react';
 import Image from 'next/image';
 import { motion, useTransform, useScroll } from 'motion/react';
 
-const cities = [
+const images = [
     {
-        src: '/images/istanbul.webp',
-        alt: 'Istanbul',
-        name: 'Istanbul'
+        src: '/images/koton/koton1.webp',
+        alt: 'Koton 1',
+        name: 'Koton 1'
     },
     {
-        src: '/images/paris.webp',
-        alt: 'Paris',
-        name: 'Paris'
+        src: '/images/koton/koton2.webp',
+        alt: 'Koton 2',
+        name: 'Koton 2'
     },
     {
-        src: '/images/cairo.webp',
-        alt: 'Cairo',
-        name: 'Cairo'
+        src: '/images/koton/koton3.webp',
+        alt: 'Koton 3',
+        name: 'Koton 3'
+    },
+    {
+        src: '/images/koton/koton4.webp',
+        alt: 'Koton 4',
+        name: 'Koton 4'
+    },
+    {
+        src: '/images/koton/koton5.webp',
+        alt: 'Koton 5',
+        name: 'Koton 5'
+    },
+    {
+        src: '/images/koton/koton6.webp',
+        alt: 'Koton 6',
+        name: 'Koton 6'
+    },
+    {
+        src: '/images/koton/koton7.webp',
+        alt: 'Koton 7',
+        name: 'Koton 7'
+    },
+    {
+        src: '/images/koton/koton8.webp',
+        alt: 'Koton 8',
+        name: 'Koton 8'
+    },
+    {
+        src: '/images/koton/koton9.webp',
+        alt: 'Koton 9',
+        name: 'Koton 9'
     }
 ]
 
@@ -30,28 +60,25 @@ const HorizontalScroll = () => {
     });
 
     // const x = useTransform(scrollYProgress, [0, 1], ["3%", "-62.5%"]);
-    const x = useTransform(scrollYProgress, [0, 1], ["0", "-164vw"]);
+    const x = useTransform(scrollYProgress, [0, 1], ["0", "-180vw"]);
 
 
     return (
         <section ref={targetRef} className="relative h-[300vh] sm:h-[400vh] lg:h-[500vh]">
             <div className="sticky top-0 h-screen flex items-center overflow-hidden ">
                 <motion.div style={{ x }} className="flex gap-[2vw] px-[10vw]">
-                    {cities.map((city, index) => (
-                        <div 
-                            key={`city${index}`}
-                            className="w-[80vw] aspect-[3/4] sm:aspect-square lg:aspect-video bg-gray-300 flex items-center justify-center"    
+                    {images.map((image, index) => (
+                        <div
+                            key={`image${index}`}
+                            className="h-[80vh] aspect-[9/16] bg-gray-300 flex items-center justify-center"
                         >
                             <Image 
-                                src={city.src}
-                                alt={city.alt}
+                                src={image.src}
+                                alt={image.alt}
                                 className='w-full h-full object-cover object-center fill'
                                 width={1920}
                                 height={1080}
                             />
-                            {/* <p className='absolute text-white text-4xl font-bold'>
-                                {city.name}
-                            </p> */}
                         </div>
                     ))}
                 </motion.div>
