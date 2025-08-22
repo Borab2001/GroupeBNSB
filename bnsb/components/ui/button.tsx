7e58ab05ef10d46d5fe59c2e97ca2a0647043cbc
Bora <borabalos9@gmail.com>
2025-08-22 17:50:29 +0200

    style: Adjusted button roundness and paddings

--- bnsb/components/ui/button.tsx
+++ bnsb/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-10 px-6 py-2 rounded-full",
         sm: "h-9 rounded-full px-3",
         lg: "h-11 rounded-full px-8 text-lg",
         icon: "h-10 w-10",
