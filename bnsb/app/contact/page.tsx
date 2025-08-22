2a74ce67f7cbe265132c35029affa8b35612cd1a
Bora <borabalos9@gmail.com>
2025-08-22 17:53:51 +0200

    style: Adjusted contact page layout

--- bnsb/app/contact/page.tsx
+++ bnsb/app/contact/page.tsx
@@ -2,18 +2,33 @@ import { ContactForm } from "@/components/contact-form";
 
 const Contact = () => {
     return (
-        <div className="w-full flex flex-1 flex-col flex-grow h-full p-4 ">
-            <div className="w-full flex flex-1 flex-col justify-center items-center">
-                <div className="flex gap-4 flex-col">
-                    <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-                        Let&apos;s get in touch
-                    </h1>
-                    <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
-                        You can reach us any time
-                    </p>
-                    <ContactForm />
-                </div>
-            </div>
+        <div className="w-full flex flex-1 flex-col justify-center min-h-hero h-full p-4 lg:p-8">
+			<div className="grid grid-cols-12 gap-8 items-center px-0 sm:px-4 lg:px-16">
+				<div className="col-span-12 sm:col-span-7 lg:col-span-6 flex flex-col justify-center">
+					<h1 className="text-6xl lg:text-7xl max-w-2xl tracking-tighter font-medium font-playfair">
+						Get in touch
+					</h1>
+					<ContactForm />
+				</div>
+				<div className="col-span-12 sm:col-span-5 lg:col-span-6 place-items-start sm:place-items-center">
+					<div className="flex flex-col items-start gap-4 sm:gap-8">
+						<div className="flex flex-col items-start gap-2">
+							<h2 className="text-base font-regular text-muted-foreground">Get help</h2>
+							<p className="text-base font-regular text-foreground">info@groupebnsb.com</p>
+						</div>
+
+						<div className="flex flex-col items-start gap-2">
+							<h2 className="text-base font-regular text-muted-foreground">Call us</h2>
+							<p className="text-base font-regular text-foreground">+90 ... ... .. ..</p>
+						</div>
+
+						<div className="flex flex-col items-start gap-2">
+							<h2 className="text-base font-regular text-muted-foreground">Option 3</h2>
+							<p className="text-base font-regular text-foreground">Tell them something</p>
+						</div>
+					</div>
+				</div>
+			</div>
         </div>
     );
 }
