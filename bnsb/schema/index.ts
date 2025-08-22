f5eab47cbb44ba2bf0121ec1c56d58495aa08d5c
Bora <borabalos9@gmail.com>
2025-08-22 16:50:27 +0200

    feat: Adding precisions to email schema

--- bnsb/schema/index.ts
+++ bnsb/schema/index.ts
@@ -1,9 +1,9 @@
 import * as z from "zod";
 
 export const EmailSchema = z.object({
-    firstName: z.string(),
-    lastName: z.string(),
-    email: z.string().email(),
-    subject: z.string(),
-    message: z.string(),
+    firstName: z.string().min(1).min(1),
+    lastName: z.string().min(1).min(1),
+    email: z.string().min(1),
+    subject: z.string().min(1).min(1).max(150),
+    message: z.string().min(1).max(1000)
 });
